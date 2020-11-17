a764f1fa67b81ad23ef22c1cf07b4cef289218f4
ChromaXard <74619256+ChromaXard@users.noreply.github.com>
2020-11-17 20:10:39 +0100

    DarxBot

---
+++ DarxBot/main.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+var bot = new Discord.Client();
+var prefix = ("/");
+
+const HelpEmbed = new MessageEmbed()
+	.setColor('#0000FF')
+	.setTitle('commande help')
+	.setURL('')
+	.setAuthor('')
+	.setDescription('Voici la liste de toutes les commandes disponible')
+	.setThumbnail('')
+	.addFields(
+        { name: '/ping', value: 'Voir si je suis actif ou non', inline: true },
+		{ name: '/help', value: 'Affiche la ce message de la commande help', inline: true },
+	)
+	.setImage('')
+	.setTimestamp()
+    .setFooter('Créateur: AlexDarx');
+
+	const PingEmbed = new MessageEmbed()
+	.setColor('#0000FF')
+	.setTitle('')
+	.setURL('')
+	.setAuthor('')
+	.setDescription('')
+	.setThumbnail('')
+	.addFields(
+        { name: 'ping?', value: 'Je suis bien actif mec tu peut utiliser mes différentes commandes', inline: true }
+	)
+	.setImage('')
+	.setTimestamp()
+	.setFooter('Créateur: AlexDarx');
+	
+bot.on('message', message => { 
+    if (message.content === prefix + "ping")
+    message.channel.send(PingEmbed)
+    if (message.content === prefix + "help")
+    message.channel.send(HelpEmbed)
+});
+
+bot.login("NzQ0MzU2MTg5MjU2Njc5NTA1.XziBuA.qDoHeBQ5l6A905qAvK7nDWwhle4")
